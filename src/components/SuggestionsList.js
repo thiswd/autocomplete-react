6f9fc145f25cb01d3de8982b0d889fe8b0d652ec
Thiago Sawada <tms1_tms2@hotmail.com>
2023-06-24 20:40:44 -0300

    feat: hover effect over suggestion list

--- src/components/SuggestionsList.js
+++ src/components/SuggestionsList.js
@@ -4,9 +4,9 @@ export default function SuggestionsList({ suggestions }) {
       data-testid="results-list"
       className="border border-primary rounded-md w-96 text-lg bg-white"
     >
-      {suggestions.map((item, i) => (
-        <li key={item.id} className="p-1">
-          <span className="">{item.title || item.name}</span>
+      {suggestions.map(item => (
+        <li key={item.id} className="p-1 hover:bg-gray-200">
+          <span>{item.title || item.name}</span>
         </li>
       ))}
     </ul>
